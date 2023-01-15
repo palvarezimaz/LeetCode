a8d8e649b8575545ed0e7aba1d3ed94890750f93
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-16 09:20:41 +1000

    Time: 74 ms (62.46%), Space: 44.6 MB (40.59%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.ts
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.ts
@@ -1,12 +1,10 @@
 function maximumWealth(accounts: number[][]): number {
     let result: number = 0
-    const partialResult: number[] = []
+    
     for (let client of accounts) {
-        partialResult.push(client.reduce((tot, acc) => { return tot + acc }))
-    }
-    for (let i: number = 0; i < partialResult.length; i++) {
-        if (partialResult[i] > result) {
-            result = partialResult[i]
+        const partialResult = client.reduce((tot, acc) => tot + acc )
+        if (partialResult > result) {
+            result = partialResult
         }
     }
     return result
