94a28c43165e5a5d312aafeeb4214008ec952403
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-17 10:26:03 +1000

    Time: 67 ms (91.77%), Space: 44.9 MB (33.90%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -0,0 +1,32 @@
+function isValid(s: string): boolean {
+    type Pairs = {
+        '}': string,
+        ']': string,
+        ')': string
+    }
+
+    const pairs: Pairs | any = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
+    const sToArray: string[] = [...s]
+    const stack: string[] = []
+    let matchingBrackets: boolean = true
+
+    sToArray.map(char => {
+        if (char === '{' || char === '[' || char === '(') {
+            stack.push(char)
+        } else {
+            if (stack[stack.length - 1] !== pairs[char]) {
+                matchingBrackets = false
+            } else {
+                stack.pop()
+            }
+        }
+    })
+    if (stack.length > 0) {
+        matchingBrackets = false
+    }
+    return matchingBrackets
+}
