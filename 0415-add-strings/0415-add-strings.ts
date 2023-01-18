6a4cfa97af7c5926b93a9e84f48ce5bc5e6b3df0
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-18 16:23:40 +1000

    Time: 70 ms (90.83%), Space: 45.8 MB (20.18%) - LeetHub

--- 0415-add-strings/0415-add-strings.ts
+++ 0415-add-strings/0415-add-strings.ts
@@ -1,28 +1,30 @@
 function addStrings(num1: string, num2: string): string {
     let result: string = ''
     let stack: number = 0
+    let stringDiff: number = Math.abs(num1.length - num2.length)
 
     if (num1.length > num2.length) {
-        let diff: string = '0'.repeat(num1.length - num2.length)
-        num2 = diff + num2
+        num2 = '0'.repeat(stringDiff) + num2
     } else {
-        let diff: string = '0'.repeat(num2.length - num1.length)
-        num1 = diff + num1
+        num1 = '0'.repeat(stringDiff) + num1
     }
+
     for (let i: number = num1.length - 1; i >= 0; --i) {
         let parcialResult: number = Number(num1[i]) + Number(num2[i]) + stack
         if (parcialResult >= 10) {
             const tempArray: string[] = (parcialResult).toString().split('')
-            result = result + tempArray[1]
+            result += tempArray[1]
             stack = Number(tempArray[0])
         }
         else {
-            result = result + parcialResult.toString()
+            result += parcialResult.toString()
             stack = 0
         }
     }
+
     if (stack > 0) {
-        result = result + stack.toString()
+        result += stack.toString()
     }
+
     return result.split('').reverse().join('')
 };
