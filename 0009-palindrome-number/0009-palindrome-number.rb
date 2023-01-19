a7321fca087b460c7ca81fd8b771ef97905130b8
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-19 22:40:05 +1000

    Time: 134 ms (57.22%), Space: 211 MB (80.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.rb
+++ 0009-palindrome-number/0009-palindrome-number.rb
@@ -1,10 +1,6 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-    num_to_array = x.to_s
-    if num_to_array == num_to_array.reverse
-        return true
-    else
-        return false
-    end 
+    num_to_ar = x.to_s
+    num_to_ar == num_to_ar.reverse
 end
