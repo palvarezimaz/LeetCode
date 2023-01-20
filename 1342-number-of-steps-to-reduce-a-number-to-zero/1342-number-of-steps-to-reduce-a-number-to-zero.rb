dc4d3253bbd4fb670a0dc71785913c754a2af8d9
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-20 20:16:08 +1000

    Time: 87 ms (60.98%), Space: 211.1 MB (17.07%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.rb
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.rb
@@ -4,11 +4,11 @@ def number_of_steps(num)
     count = 0
     num_to_reduce = num.to_i
     while num_to_reduce > 0
-        if num_to_reduce % 2 != 0 
-            num_to_reduce = (num_to_reduce - 1)
+        if num_to_reduce.odd? 
+            num_to_reduce -= 1
             count = count += 1
         else
-            num_to_reduce = num_to_reduce / 2
+            num_to_reduce /= 2
             count += 1
         end
     end
