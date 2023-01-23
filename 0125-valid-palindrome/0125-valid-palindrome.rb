b6ad95e3d588fa1781301edbf2b1653c33b4fcdd
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-23 15:59:06 +1000

    Time: 303 ms (13.33%), Space: 212.9 MB (24.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rb
+++ 0125-valid-palindrome/0125-valid-palindrome.rb
@@ -1,6 +1,5 @@
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
-    to_evaluate = s.downcase.gsub(/[^0-9a-z]/i,'')
-    to_evaluate == to_evaluate.reverse
+    s.downcase.gsub(/[^0-9a-z]/i,'') == s.downcase.gsub(/[^0-9a-z]/i,'').reverse
 end
