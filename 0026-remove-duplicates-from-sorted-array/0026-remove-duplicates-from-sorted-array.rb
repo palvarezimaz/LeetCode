6dbc80602c8b8a48c913894466a0dc4149c69c10
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-25 09:10:50 +1000

    Time: 91 ms (82.76%), Space: 211.8 MB (49.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
@@ -1,10 +1,14 @@
-function removeDuplicates(nums: number[]): number {
-    let count: number = 0
-    for (let i: number = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i + 1]) {
-            nums[count] = nums[i]
-            count++
-        }
-    }
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+    count = 0
+    (0...nums.length - 1).each do |index|
+        if nums[index] != nums[index + 1]
+            nums[count] = nums[index]
+            count += 1
+        end
+    end
+    nums[count] = nums[nums.length - 1]
+    count += 1
     return count
-};
+end
