9157766c486934143863e2e2ca8cf91ee030c4d5
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-27 09:35:36 +1000

    Time: 115 ms (8.19%), Space: 42.7 MB (87.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,4 +1,4 @@
 function lengthOfLastWord(s: string): number {
-    const sToArray: string[] = s.split(' ').filter(e => e.length !== 0)
+    const sToArray: string[] = s.trim().split(' ')
     return sToArray[sToArray.length - 1].length
 };
