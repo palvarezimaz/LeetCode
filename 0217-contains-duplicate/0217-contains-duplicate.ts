436d7b341e27e926e863a4aedf8b2b8de21ed31e
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-26 22:38:01 +1000

    Time: 186 ms (20.81%), Space: 52.8 MB (20.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,5 +1,7 @@
 function containsDuplicate(nums: number[]): boolean {
-    nums.sort()
+    nums.sort(function  (a, b) {
+      return a - b;
+    });
     for (let i: number = 0; i < nums.length; i++) {
         if (nums[i] === nums[i + 1]) {
             return true
