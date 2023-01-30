e74f7c03179575d36e52f891d99363b776654cf9
Pablo A. I <71609716+palvarezimaz@users.noreply.github.com>
2023-01-30 12:25:44 +1000

    Time: 69 ms (97.56%), Space: 44.9 MB (57.72%) - LeetHub

--- 0258-add-digits/0258-add-digits.ts
+++ 0258-add-digits/0258-add-digits.ts
@@ -3,13 +3,11 @@ function addDigits(num: number): number {
         return num
     } else {
         let partialResult: number = 0
-        const numToStringArray: string[] = num.toString().split('')
-        
+        const numToStringArray: string[] = num.toString().split('')  
         partialResult = numToStringArray.reduce((tot, acc) => tot + parseInt(acc), 0)
         while (partialResult > 9) {
             const numToStringArray: string[] = partialResult.toString().split('')
             partialResult = numToStringArray.reduce((tot, acc) => tot + parseInt(acc), 0)
-
         }
         return partialResult
     }
